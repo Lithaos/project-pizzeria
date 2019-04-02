4ae072b4069f4c9a0c20481d3fa35807ef5d93d1
Wojciech Raczynski <wojto3@op.pl>
2019-04-02 13:01:46 +0200

    add booking

--- src/js/components/Booking.js
+++ src/js/components/Booking.js
@@ -3,7 +3,8 @@ import {
 } from './AmountWidget.js';
 import {
   templates,
-  select
+  select,
+  settings
 } from '../settings.js';
 import {
   DatePicker
@@ -11,6 +12,9 @@ import {
 import {
   HourPicker
 } from './HourPicker.js';
+import {
+  utils
+} from '../utils.js';
 
 export class Booking {
   constructor(widget) {
@@ -18,6 +22,7 @@ export class Booking {
 
     thisBooking.render(widget);
     thisBooking.initWidgets();
+    thisBooking.getData();
   }
 
   render(widget) {
@@ -41,5 +46,70 @@ export class Booking {
     thisBooking.hoursAmount = new AmountWidget(thisBooking.dom.hoursAmount);
     thisBooking.datePicker = new DatePicker(thisBooking.dom.datePicker);
     thisBooking.hourPicker = new HourPicker(thisBooking.dom.hourPicker);
+  }
+
+  getData() {
+    const thisBooking = this;
+
+    const startEndDates = {};
+    startEndDates[settings.db.dateStartParamKey] = utils.dateToStr(thisBooking.datePicker.minDate);
+    startEndDates[settings.db.dateEndParamKey] = utils.dateToStr(thisBooking.datePicker.maxDate);
+
+    const endDate = {};
+    endDate[settings.db.dateEndParamKey] = startEndDates[settings.db.dateEndParamKey];
+
+    const params = {
+      booking: utils.queryParams(startEndDates),
+      eventsCurrent: settings.db.notRepeatParam + '&' + utils.queryParams(startEndDates),
+      eventsRepeat: settings.db.repeatParam + '&' + utils.queryParams(endDate),
+    };
+    const urls = {
+      booking: settings.db.url + '/' + settings.db.booking + '?' + params.booking,
+      eventsCurrent: settings.db.url + '/' + settings.db.event + '?' + params.eventsCurrent,
+      eventsRepeat: settings.db.url + '/' + settings.db.event + '?' + params.eventsRepeat,
+    };
+
+    Promise.all([fetch(urls.booking),fetch(urls.eventsCurrent),fetch(urls.eventsRepeat)])
+      .then(function ([bookingsResponse, eventsCurrentResponse, eventsRepeatResponse]) {
+        return Promise.all([
+          bookingsResponse.json(),
+          eventsCurrentResponse.json(),
+          eventsRepeatResponse.json(),
+        ]);
+      })
+      .then(function ([bookings, eventsCurrent, eventsRepeat]) {
+        thisBooking.parseData(bookings, eventsCurrent, eventsRepeat);
+      });
+  }
+
+  parseData(bookings, eventsCurrent, eventsRepeat) {
+    const thisBooking = this;
+    thisBooking.booked = {};
+    for (let event of eventsCurrent) {
+      thisBooking.makeBooked(event.date, event.hour, event.duration, event.table);
+    }
+    for (let booking of bookings) {
+      thisBooking.makeBooked(booking.date, booking.hour, booking.duration, booking.table);
+    }
+    for (let eventRepeat of eventsRepeat) {
+      for (let i = 0; i < settings.datePicker.maxDaysInFuture; i++) {
+        thisBooking.makeBooked(utils.dateToStr(utils.addDays(thisBooking.datePicker.minDate, i)), eventRepeat.hour, eventRepeat.duration, eventRepeat.table);
+      }
+    }
+  }
+
+  makeBooked(date, hour, duration, tableId) {
+    const thisBooking = this;
+    const hourAsNumber = utils.hourToNumber(hour);
+    if (!thisBooking.booked.hasOwnProperty(date)) {
+      thisBooking.booked[date] = {};
+    }
+    for (let i = 0; i < duration * 2; i++) {
+      if (!thisBooking.booked[date].hasOwnProperty(hourAsNumber + i * 0.5)) {
+        thisBooking.booked[date][hourAsNumber + i * 0.5] = [tableId];
+      } else {
+        thisBooking.booked[date][hourAsNumber + i * 0.5].push(tableId);
+      }
+    }
   }
 }
