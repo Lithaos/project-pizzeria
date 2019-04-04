a9ffc1244368b37a5bed6de7af0fa30d8956befb
Wojciech Raczynski <wojto3@op.pl>
2019-04-04 10:06:06 +0200

    add booking table

--- src/js/settings.js
+++ src/js/settings.js
@@ -64,6 +64,7 @@ export const select = {
     peopleAmount: '.people-amount',
     hoursAmount: '.hours-amount',
     tables: '.floor-plan .table',
+    bookingSubmit: '.order-confirmation [type="submit"]',
   },
   nav: {
     links: '.main-nav a',
